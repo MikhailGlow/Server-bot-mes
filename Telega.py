fcfd0517457dd4c4f27ab2df7eb691a5ba3118f0
Neonexer <65070875+Neonexer@users.noreply.github.com>
2022-07-06 18:21:24 +0700

    test init

--- Telega.py
+++ Telega.py
@@ -5,9 +5,11 @@ bot = telebot.TeleBot("5468093435:AAHl63kZ2gG6Uqxija0E0lYXmxEL-6-4bnk", parse_mo
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
 	bot.reply_to(message, "Howdy, how are you doing?")
+	
 
 @bot.message_handler(func=lambda m: True)
 def echo_all(message):
 	bot.reply_to(message, message.text)
+	
 
 bot.infinity_polling()
