0eaf97222bb190cf94e9603edf30a1e57e059b38
Neonexer <65070875+Neonexer@users.noreply.github.com>
2022-07-06 18:19:07 +0700

    Add files via upload

---
+++ Telega.py
@@ -0,0 +1,13 @@
+import telebot
+
+bot = telebot.TeleBot("5468093435:AAHl63kZ2gG6Uqxija0E0lYXmxEL-6-4bnk", parse_mode=None)
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+	bot.reply_to(message, "Howdy, how are you doing?")
+
+@bot.message_handler(func=lambda m: True)
+def echo_all(message):
+	bot.reply_to(message, message.text)
+
+bot.infinity_polling()
